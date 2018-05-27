99ce9e19e1e50db9cd7c38357ebcda6b2fa02ccf
Bartosz Wojtaś <el.klides@op.pl>
2018-05-27 15:44:43 +0200

    Change database connection

--- server/config.js
+++ server/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/mern-starter',
+  mongoURL: process.env.MONGO_URL || 'mongodb://admin:kanban@ds137600.mlab.com:37600/kanban',
   port: process.env.PORT || 8000,
 };
 
--- server/server.js
+++ server/server.js
@@ -47,9 +47,7 @@ import serverConfig from './config';
 mongoose.Promise = global.Promise;
 
 // MongoDB Connection
-//mongoose.connect(serverConfig.mongoURL, (error) => {
-	
-mongoose.connect('mongodb://admin:kanban@ds137600.mlab.com:37600/kanban', (error) => {
+mongoose.connect(serverConfig.mongoURL, (error) => {
   if (error) {
     console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
     throw error;
